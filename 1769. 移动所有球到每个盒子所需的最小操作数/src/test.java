a861c28acf7c2990072c4073f1504feff7fdb1e7
jackyzou <jackyzou2510@gmail.com>
2024-03-31 09:10:27 +0800

    3/31

---
+++ 1769. 移动所有球到每个盒子所需的最小操作数/src/test.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Jacky Zou
+ * @Date 2024/3/18 16:15
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] minOperations(String boxes) {
+
+    }
+}
